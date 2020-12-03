8eaf729d098e84b0c8316c0dca12fed4ce2ae6c1
digitros <dgogo9@hotmail.com>
2020-12-03 15:27:53 -0600

    Updating the files structure

---
+++ src/routes/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
+const App = () => {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={} />
+        </Switch>
+      </BrowserRouter>
+    );
+}
+
+export default App;
