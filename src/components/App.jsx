584ae9fd756dab3d4b4d7ef678ed68a681d3938d
digitros <dgogo9@hotmail.com>
2020-12-03 14:30:41 -0600

    Initial configuration for a React project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/components/App.css'
+
+const App = () => {
+    return (
+      <div>
+        <h1>Hola mundo</h1>
+      </div>
+    );
+}
+
+export default App;
